0dafd37fcb1fbe682b3df5a62296a2bd2a575565
joeeey <joeeey.jo.ez@gmail.com>
2025-01-31 11:17:05 +0700

    renamed:    projectRSM/db.sqlite3 -> ProjectSRM/app/__init__.py new file:   ProjectSRM/app/admin.py new file:   ProjectSRM/app/apps.py new file:   ProjectSRM/app/migrations/__init__.py new file:   ProjectSRM/app/models.py new file:   ProjectSRM/app/tests.py new file:   ProjectSRM/app/views.py new file:   ProjectSRM/core/__init__.py new file:   ProjectSRM/core/asgi.py new file:   ProjectSRM/core/settings.py new file:   ProjectSRM/core/urls.py new file:   ProjectSRM/core/wsgi.py new file:   ProjectSRM/manage.py new file:   django.log new file:   midProjectRSM/django.log modified:   midProjectRSM/django_pymisp.log modified:   midProjectRSM/manage.py deleted:    projectRSM/apps/__pycache__/__init__.cpython-313.pyc deleted:    projectRSM/apps/authentication/__pycache__/__init__.cpython-313.pyc deleted:    projectRSM/apps/authentication/__pycache__/forms.cpython-313.pyc deleted:    projectRSM/apps/authentication/__pycache__/urls.cpython-313.pyc deleted:    projectRSM/apps/authentication/__pycache__/views.cpython-313.pyc deleted:    projectRSM/apps/home/__pycache__/__init__.cpython-313.pyc deleted:    projectRSM/apps/home/__pycache__/admin.cpython-313.pyc deleted:    projectRSM/apps/home/__pycache__/models.cpython-313.pyc deleted:    projectRSM/apps/home/__pycache__/urls.cpython-313.pyc deleted:    projectRSM/apps/home/__pycache__/views.cpython-313.pyc deleted:    projectRSM/apps/home/migrations/__pycache__/__init__.cpython-313.pyc deleted:    projectRSM/core/__pycache__/__init__.cpython-313.pyc deleted:    projectRSM/core/__pycache__/settings.cpython-313.pyc deleted:    projectRSM/core/__pycache__/urls.cpython-313.pyc deleted:    projectRSM/core/__pycache__/wsgi.cpython-313.pyc

--- ProjectSRM/manage.py
+++ ProjectSRM/manage.py
@@ -2,16 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import logging
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    handlers=[
-        logging.FileHandler("django.log"),
-        logging.StreamHandler()
-    ]
-)
 
 def main():
     """Run administrative tasks."""
@@ -19,16 +10,13 @@ def main():
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        logging.critical("Django import failed. Check if Django is installed and virtualenv is activated.", exc_info=True)
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-
-    logging.info("Executing Django command: %s", " ".join(sys.argv))
     execute_from_command_line(sys.argv)
+
 
 if __name__ == '__main__':
     main()
-
