e1686411510d3c006f4d6c7b126bc493b3278f1f
joeeey <joeeey.jo.ez@gmail.com>
2025-02-02 19:38:59 +0700

    modified:   ProjectSRM/app/__pycache__/__init__.cpython-313.pyc new file:   ProjectSRM/app/__pycache__/admin.cpython-313.pyc new file:   ProjectSRM/app/__pycache__/apps.cpython-313.pyc new file:   ProjectSRM/app/__pycache__/forms.cpython-313.pyc new file:   ProjectSRM/app/__pycache__/models.cpython-313.pyc modified:   ProjectSRM/app/__pycache__/urls.cpython-313.pyc modified:   ProjectSRM/app/__pycache__/views.cpython-313.pyc new file:   ProjectSRM/app/__pycache__/virustotal_service.cpython-313.pyc new file:   ProjectSRM/app/migrations/__pycache__/0001_initial.cpython-313.pyc new file:   ProjectSRM/app/migrations/__pycache__/0002_vtresult.cpython-313.pyc new file:   ProjectSRM/app/migrations/__pycache__/__init__.cpython-313.pyc modified:   ProjectSRM/app/models.py modified:   ProjectSRM/core/__pycache__/__init__.cpython-313.pyc modified:   ProjectSRM/core/__pycache__/settings.cpython-313.pyc modified:   ProjectSRM/core/__pycache__/urls.cpython-313.pyc modified:   ProjectSRM/core/__pycache__/wsgi.cpython-313.pyc modified:   ProjectSRM/core/settings.py modified:   ProjectSRM/core/templates/form1.html modified:   ProjectSRM/core/templates/index.html

--- ProjectSRM/app/models.py
+++ ProjectSRM/app/models.py
@@ -10,7 +10,6 @@ class RiskAssessment(models.Model):
     asset_description = models.TextField()
     likelihood = models.CharField(max_length=50)
     impact = models.CharField(max_length=50)
-    residual_risk = models.CharField(max_length=50)
     existing_controls = models.TextField()
     additional_controls = models.TextField()
     review_frequency = models.CharField(max_length=50)
